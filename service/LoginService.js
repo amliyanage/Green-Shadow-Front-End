bcc04d9fc93b424ae50c0c8191dac5affd49d679
amliyanage <ashenmliyanage@gmail.com>
2024-11-17 14:14:02 +0530

    connect register form to backend

---
+++ service/LoginService.js
@@ -0,0 +1,24 @@
+// In LoginService.js
+export function register(email, password, role) {
+  const raw = {
+    email: email,
+    password: password,
+    role: role,
+  };
+
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: "http://localhost:5055/greenshadow/v1/auth/signup",
+      type: "POST",
+      contentType: "application/json",
+      data: JSON.stringify(raw),
+      success: function (result) {
+        console.log(result);
+        resolve(result); // resolving with the response result
+      },
+      error: function (xhr, status, error) {
+        reject(error); // rejecting on error
+      },
+    });
+  });
+}
